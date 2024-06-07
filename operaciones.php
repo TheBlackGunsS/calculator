8ce16da8a2a2d4899cef8a2a0137d1e889cc6639
TheBlackGunsS <Datl01@outlook.com>
2024-06-07 00:24:56 -0600

    sumatoria de dos numeros

---
+++ operaciones.php
@@ -0,0 +1,9 @@
+<?php
+// Función para sumar dos números
+function sumar($a, $b) {
+    return $a + $b;
+}
+
+// Realizar las operaciones y mostrar los resultados
+echo "Suma de $num1 y $num2: " . sumar($num1, $num2) . "\n";
+?>
