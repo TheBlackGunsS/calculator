fcb10a63a5928f1e31c93edb1f103cb8fdf0b9e3
TheBlackGunsS <Datl01@outlook.com>
2024-06-07 00:40:06 -0600

    hotfix - definicion de los numeros

--- operaciones.php
+++ operaciones.php
@@ -4,6 +4,10 @@ function sumar($a, $b) {
     return $a + $b;
 }
 
+// Definir dos números
+$num1 = 10;
+$num2 = 5;
+
 // Realizar las operaciones y mostrar los resultados
 echo "Suma de $num1 y $num2: " . sumar($num1, $num2) . "\n";
 ?>
